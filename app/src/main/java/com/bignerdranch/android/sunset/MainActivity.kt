5002bdf77a6c68dbd817af32d5c9361bae31abf8
Roland <nnamdiigede11@gmail.com>
2022-03-10 19:30:32 +0100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.sunset
+
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var sceneView: View
+    private lateinit var sunView: View
+    private lateinit var skyView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        sceneView = findViewById(R.id.scene)
+        sunView = findViewById(R.id.sun)
+        skyView = findViewById(R.id.sky)
+        sceneView.setOnClickListener {
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = sunView.top.toFloat()
+        val sunYEnd = skyView.height.toFloat()
+        val heightAnimator = ObjectAnimator
+            .ofFloat(sunView, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.start()
+    }
+}
